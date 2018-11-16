73a76e1391c897a6bf753844df1b2a3fc757fcb6
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-11-16 10:17:25 -0700

    Added the hash tables ex1. Throwing errors, will return to it if time

--- hash-tables/ex1/ex1.c
+++ hash-tables/ex1/ex1.c
@@ -7,7 +7,27 @@ Answer *get_indices_of_item_weights(int *weights, int length, int limit)
 {
   HashTable *ht = create_hash_table(16);
 
-  // YOUR CODE HERE
+  if(length == 1){
+    return NULL; 
+  }
+  
+  // Filling hash table with weight data key and index as value
+  for(int i = 0; i < length; i++){
+    hash_table_insert(ht, weights[i], i); 
+  }
+
+  Answer *answer = NULL; 
+
+  for(int i = 0; i < length; i++){
+    int weight2 = limit - weights[i]; 
+    if(hash_table_retrieve(ht, weight2) != NULL){
+      answer-> index_1 = i;
+      answer-> index_2 = hash_table_retrieve(ht, weight2); 
+    }
+  }
+
+  print_answer(answer); 
+
 
   return NULL;
 }
