c05758ab92dd39188200f16efc63fc0d205df6b7
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-11-16 11:59:41 -0700

    ex2 implementation complete

--- hash-tables/ex2/ex2.c
+++ hash-tables/ex2/ex2.c
@@ -15,7 +15,13 @@ char **reconstruct_trip(Ticket **tickets, int length)
   }
   // populate routes array
   //first find the first element in the array by locating the key which value is NULL
+  route[0] = hash_table_retrieve(hash, NULL); 
   // to find second value and other remaining values, save destination of first, then search hash for the first array value
+  for(int i = 1; i < length; i++){
+    route[i] = hash_table_retrieve(hash, route[i-1])
+  }
+  //destroy hash table and clear memory when done
+  destroy_hash_table(hash); 
   return route;
 }
 
